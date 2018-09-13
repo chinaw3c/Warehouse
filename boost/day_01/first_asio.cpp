7156eff6bf60da307e7dcc3eed289a4303df8cd0
chinaw3c <chinaw3c@w4ctech.cn>
2018-09-13 20:48:22 +0800

    可以实现客户端连接服务器，会返回一个消息

--- boost/day_01/first_asio.cpp
+++ boost/day_01/first_asio.cpp
@@ -20,23 +20,19 @@ int main(int argc, char * argv[])
             std::cerr << "Usage: client <host>" << std::endl;
             return 1;
         }
-
+        
         boost::asio::io_service io_service;
         tcp::resolver resolver(io_service);
-<<<<<<< HEAD
         tcp::resolver::query query(argv[1], "23333");
-=======
-        tcp::resolver::query query(argv[1], "daytime");
->>>>>>> 5c29814d15e086decb475e7f399cd0bccdd4542a
         tcp::resolver::iterator endpoint_iterator = resolver.resolve(query);
-
+        
         tcp::socket socket(io_service);
         boost::asio::connect(socket, endpoint_iterator);
-
+        
         while (true){
             boost::array<char, 128> buf;
             boost::system::error_code error;
-
+            
             size_t len = socket.read_some(boost::asio::buffer(buf), error);
             if (error == boost::asio::error::eof){
                 break;
